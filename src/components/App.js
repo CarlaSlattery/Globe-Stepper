0a41e9ab9d17893ecb59f74a4bbe926eb5e5665f
CarlaSlattery <cslatterydev@gmail.com>
2023-02-27 12:07:28 +0000

    navbar routing set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 import React from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
+// Component imports
+import RootLayout from "./RootLayout";
+import Home from "./Home";
+import Login from "./Login";
+
+// Style imports
 import "../styles/App.css";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <div className="App">
-      <h1>GlobeStepper</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
