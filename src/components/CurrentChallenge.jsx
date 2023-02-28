7347a11cbb14d188b1fb9386648bd862add803ad
CarlaSlattery <cslatterydev@gmail.com>
2023-02-28 21:41:31 +0000

    added progress tracker form - useState working on post distance input field

--- src/components/CurrentChallenge.jsx
+++ src/components/CurrentChallenge.jsx
@@ -1,22 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Component import
 import ChallengeCard from "./ChallengeCards";
 
+// Imported styling
+import "../styles/current-challenge.css";
+
 function CurrentChallenge() {
+  const [distance, setNewDistance] = useState("");
   return (
     <div className="challenge-container">
       <div className="image-container">
         <div className="progress-container">
           <h3>Progress Tracker</h3>
-          <img
-            className="progress-graphic"
-            src="#"
-            alt="percentage completed"
-          />
+          <label htmlFor="progress percentage">
+            Challenge Completion Percentage
+            <progress
+              id="progressPercent"
+              className="progress-bar"
+              max="100"
+              value="65"
+            >
+              65%
+            </progress>
+          </label>
+
+          <form className="addDistance">
+            <label htmlFor="distance entry">
+              Post a Distance:
+              <input
+                id="addDistance"
+                type="number"
+                placeholder="0.00"
+                required
+                value={distance}
+                onChange={(e) => setNewDistance(e.target.value)}
+              />
+            </label>
+            <button className="enter-distance-btn" type="submit">
+              Post
+            </button>
+            <p>{distance}</p>
+          </form>
+
           <div className="challenge-statistics">
             <span>Distance Travelled:</span>
+            <span>76km</span>
             <span>Distance Remaining:</span>
+            <span>41km</span>
           </div>
         </div>
         <div className="completed-challenges">
