8150944e84f12e5b2c284ae4faa969a5b7b17616
CarlaSlattery <cslatterydev@gmail.com>
2023-03-03 13:41:52 +0000

    restructured navbar, logo added

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -2,20 +2,24 @@ import React from "react";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { NavLink, Outlet } from "react-router-dom";
 
+import globeStepper from "../assets/GlobeStepper.jpg";
+
 function RootLayout() {
   return (
     <div className="app-root-layout">
-      <header>
-        <nav>
-          <span className="logo">GlobeStepperLogo</span>
-          <NavLink to="/" activeClassName="active">
-            Challenges
-          </NavLink>
-          <NavLink to="login" activeClassName="active">
-            Login
-          </NavLink>
-        </nav>
-      </header>
+      <nav className="navigation">
+        <img className="navbar-logo" src={globeStepper} alt="logo" />
+        <div className="navigation-menu">
+          <ul>
+            <li>
+              <NavLink to="/">Challenges</NavLink>
+            </li>
+            <li>
+              <NavLink to="login">Login</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
       <main>
         <Outlet />
       </main>
