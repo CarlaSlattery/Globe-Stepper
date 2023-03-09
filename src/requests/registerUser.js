367025a161c111e15bdae2a449ae704d8010ee07
Deryn Rushworth <derynrushworth@gmail.com>
2023-03-09 07:24:21 +0000

    created register user request, updated axios in dependencies

---
+++ src/requests/registerUser.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const registerUser = (fields, setAlert) => {
+  return axios
+    .post("http://localhost:4000/users", fields)
+    .then(() => {
+      setAlert({
+        message: "Registration successful!",
+        success: true,
+      });
+    })
+    .catch(() => {
+      setAlert({
+        message: "Server error - please try again later.",
+        success: false,
+      });
+    });
+};
+
+export default registerUser;
