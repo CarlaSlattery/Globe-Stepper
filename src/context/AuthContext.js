7fd5e39b577ea04dca25e47457e0395fea9d79c5
CarlaSlattery <cslatterydev@gmail.com>
2023-03-11 17:47:57 +0000

    amended AuthContext function, added hook - needs linking with forms

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,13 +1,30 @@
 import React, { createContext, useReducer } from "react";
 
 export const AuthContext = createContext();
+export const initialState = {
+  isAuthenticated: false,
+  user: null,
+  token: null,
+};
 
 export const authReducer = (state, action) => {
   switch (action.type) {
     case "LOGIN":
-      return { user: action.payload };
+      localStorage.setItem("user", JSON.stringify(action.payload.user));
+      localStorage.setItem("token", JSON.stringify(action.payload.token));
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+        token: action.payload.token,
+      };
     case "LOGOUT":
-      return { user: null };
+      localStorage.clear();
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
     default:
       return state;
   }
