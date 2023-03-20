a4fc0d1148b2f8bfd78c1a57062a0387fbf86d89
CarlaSlattery <cslatterydev@gmail.com>
2023-03-20 18:07:56 +0000

    chakra added to index.js file - now rendering across app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import "./styles/index.css";
 import App from "./components/App";
 import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </React.StrictMode>
+  <ChakraProvider>
+    <React.StrictMode>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </React.StrictMode>
+  </ChakraProvider>
 );
