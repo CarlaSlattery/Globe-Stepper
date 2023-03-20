e3621a8b244339a168545af1568d2f039560de63
CarlaSlattery <cslatterydev@gmail.com>
2023-03-20 10:34:24 +0000

    amended home page heading typography - need font import

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,13 +12,33 @@ function Home() {
   return (
     <Container maxW="100%">
       <Box bg="blue.400" w="100%">
-        <Heading as="h1" fontSize="6xl">
+        <Heading
+          as="h1"
+          fontSize="6xl"
+          textAlign="center"
+          p="0.25em"
+          color="white"
+        >
           Globestepper
         </Heading>
-        <Text fontSize="3xl">Where will you walk?</Text>
+        <Text
+          fontSize="3xl"
+          textAlign="center"
+          marginBottom="0.75em"
+          fontStyle="italic"
+        >
+          Where will you walk?
+        </Text>
       </Box>
-      <Container maxW="60%" bg="green.300" mt="2em" px="3em" py="1em">
-        <Text>
+      <Container
+        maxW="60%"
+        bg="green.300"
+        mt="2em"
+        mb="2.5em"
+        px="3em"
+        py="1em"
+      >
+        <Text fontSize="1.25rem">
           Join a challenge and walk the distance of a famous route or landmark.
           Choose from 9 different challenges with different difficulty levels
           based on distance. Each day enter the total distance you have walked
